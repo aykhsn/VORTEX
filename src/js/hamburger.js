b3c8383b60b643c3e04057303207b3bdccd0f9b7
aykhsn <ayakah005@gmail.com>
2022-11-01 21:42:47 +0900

    Update Header

---
+++ src/js/hamburger.js
@@ -0,0 +1,9 @@
+const hamburgerButton = document.querySelector('#js-hamburger');
+// const nav = document.querySelector('#js-nav');
+
+hamburgerButton.addEventListener('click', function () {
+
+  hamburgerButton.classList.toggle('active');
+  // nav.classList.toggle('active');
+
+});
